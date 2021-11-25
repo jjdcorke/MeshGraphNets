321581021426623b60a1b7d5e052182bf98f3bbd
j3ruther <91139760+j3ruther@users.noreply.github.com>
2021-11-24 21:03:37 -0500

    Add files via upload

--- Visualization/plot_cfd.py
+++ Visualization/plot_cfd.py
@@ -13,7 +13,9 @@ from matplotlib import tri as mtri
 
 
 def plot_cfd(data, filename):
-    figure, axis = plt.subplots(1, 1, figsize=(12, 8))
+    figure =plt.figure(figsize = (12,6))
+    axis = figure.add_subplot(121)
+    axis2 = figure.add_subplot(122)
     skip = 10
     num_steps = data[0]['gt_velocity'].shape[0]
     num_frames = len(data) * num_steps // skip
@@ -37,9 +39,26 @@ def plot_cfd(data, filename):
          faces = data[traj]['faces'][step]
          velocity = data[traj]['pred_velocity'][step]
          triang = mtri.Triangulation(pos[:, 0], pos[:, 1], faces)
-         axis.tripcolor(triang, velocity[:, 0], vmin=vmin[0], vmax=vmax[0])
+         axis.tripcolor(triang, velocity[:, 0], velocity[:, 1], vmin=vmin[0], vmax=vmax[0])
          axis.triplot(triang, 'ko-', ms=0.5, lw=0.3)
-         axis.set_title('Trajectory %d Step %d' % (traj, step))
+         axis.set_title("Predicted")
+         
+         step = (frame*skip) % num_steps
+         traj = (frame*skip) // num_steps
+         axis2.cla()
+         axis2.set_aspect('equal')
+         axis2.set_axis_off()
+         vmin, vmax = bounds[traj]
+         pos = data[traj]['mesh_pos'][step]
+         faces = data[traj]['faces'][step]
+         velocity = data[traj]['gt_velocity'][step]
+         triang = mtri.Triangulation(pos[:, 0], pos[:, 1], faces)
+         axis2.tripcolor(triang, velocity[:, 0], velocity[:, 1], vmin=vmin[0], vmax=vmax[0])
+         axis2.triplot(triang, 'ko-', ms=0.5, lw=0.3)
+         axis2.set_title("Ground Truth")
+         
+         figure.suptitle(f"Time step: {frame}")
+         
          return figure,
 
     ani = animation.FuncAnimation(figure, animate, frames=num_frames, interval=100)
